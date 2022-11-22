ffd1c702b211285e7bb38f9148e5b52af4c3996a
Vijay Benz <benzwrites@gmail.com>
2022-11-22 17:27:55 +0530

    Create printReverseN.java

---
+++ Java/recursion/printReverseN.java
@@ -0,0 +1,15 @@
+class primeRange {
+    public static void main(String[] args) {
+
+        printrev(5);
+    }
+    static void printrev(int n){
+        if(n==1){
+            System.out.print(n);
+        }
+        else{
+            System.out.print(n + " ");
+            printrev(n-1);
+        }
+    }
+}
