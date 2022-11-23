c25a6e7763786219d3b0277f91c59c611049a871
Vijay Benz <benzwrites@gmail.com>
2022-11-23 17:16:48 +0530

    Update sumOfDigits.java

--- Java/recursion/sumOfDigits.java
+++ Java/recursion/sumOfDigits.java
@@ -1,16 +1,16 @@
 public class sumOfDigits {
     public static void main(String[] args) {
-        System.out.println(function(7437,1000));
+        System.out.println(function(7437));
     }
-    static int function(int n,int digits){
+    static int function(int n){
         if(n==0){
             return 0;
         }
         int sum = 0;
 
-        sum = n/digits + sum;
+        sum = n%10+ sum;
 
 
-        return sum+function(n%digits, digits/10);
+        return sum+function(n/10);
     }
 }
