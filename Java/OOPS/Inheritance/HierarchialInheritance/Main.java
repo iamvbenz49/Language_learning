6b84eeb6fdb5c840f7e0178fc42c99f8e6e2ddc4
Sham Vijay <benzwrites@gmail.com>
2022-12-24 23:14:58 +0530

    Create Main.java

---
+++ Java/OOPS/Inheritance/HierarchialInheritance/Main.java
@@ -0,0 +1,11 @@
+class Main{
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+/*
+  SINGLE INHERITANCE - only one class extends
+  MULTILEVEL INHERITANCE - more than one class extends
+  MULTIPLE INHERITANCE - not possible in java
+  HIERARCHIAL INHERITANCE - more than one child extends the same parent
+  HYBRID INHERITANCE - combination of single and multi level inheritance,But in java its not possible */
