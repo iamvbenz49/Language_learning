2a304e69597525efca650d2d131698c9678810a4
Sham Vijay <benzwrites@gmail.com>
2023-01-14 10:43:06 +0530

    Update bubbleSort.java

--- Java/sorting/bubbleSort.java
+++ Java/sorting/bubbleSort.java
@@ -9,7 +9,7 @@ public class bubbleSort {
     }
     public static void sort(int[] arr){
         for(int i=0;i<arr.length-1;i++){
-            for(int j=i+1;j<arr.length;j++){
+            for(int j=1;j<arr.length-i;j++){
                 if(arr[i]>arr[j]){
                     swap(arr,i,j);
                 }
