8a093261629fd7476cc16b4e6fa3245deb6f3d05
Sham Vijay <benzwrites@gmail.com>
2023-05-31 11:49:41 -0400

    Create functionAsArguement.c

---
+++ C/pointers/functionAsArguement.c
@@ -0,0 +1,16 @@
+//Using Pointer 
+void printArray(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+//using array
+int main() {
+    int myArray[] = {1, 2, 3, 4, 5};
+    int size = sizeof(myArray) / sizeof(myArray[0]);
+    printArray(myArray, size);
+    return 0;
+}
+
+//but in both case it is going to point to the first element of an array
